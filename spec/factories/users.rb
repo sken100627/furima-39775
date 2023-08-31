926e0c78505c0f4edcfb58dddcdbf0080b78850d
sken100627 <shigujianren01@gmail.com>
2023-08-31 11:40:26 +0900

    ユーザー情報モデル単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name { Gimei.name.last.kanji }
+    first_name { Gimei.name.first.kanji }
+    last_name_kana { Gimei.name.last.katakana }
+    first_name_kana { Gimei.name.first.katakana }
+    birthday   {Faker::Date.birthday}
+  end
+end
