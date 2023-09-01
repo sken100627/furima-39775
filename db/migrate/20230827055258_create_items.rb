cec9135ef3c3233846d70ed33477240d5530510c
sken100627 <shigujianren01@gmail.com>
2023-09-01 10:58:40 +0900

    itemsテーブル作成

--- db/migrate/20230827055258_create_items.rb
+++ db/migrate/20230827055258_create_items.rb
@@ -1,6 +1,14 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
+      t.string  :name,               null: false, default: ""
+      t.text    :explanation,        null: false, default: ""
+      t.integer :category_id,        null: false, default: ""
+      t.integer :condition_id,       null: false, default: ""
+      t.integer :shipping_charge_id, null: false, default: ""
+      t.integer :prefecture_id,      null: false, default: ""
+      t.integer :price,              null: false, default: ""
+      t.integer :user_id,            null: false, default: ""
 
       t.timestamps
     end
