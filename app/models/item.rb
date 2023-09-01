f34afb3f227e7926e29921419eb57ba3f6d2500c
sken100627 <shigujianren01@gmail.com>
2023-09-01 11:59:34 +0900

    Active Hashのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
