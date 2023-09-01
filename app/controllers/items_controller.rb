5e81cf09ba8feb2dfb539cb3c25d1d2c635bc98c
sken100627 <shigujianren01@gmail.com>
2023-09-01 19:39:32 +0900

    itemモデルとfactorybotの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:explanation,:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:shipping_day_id,:price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:name,:explanation,:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:shipping_day_id,:price).merge(user_id: current_user.id)
   end
 
   def move_to_user_session
