4e452748fcbf3dce1d5bc7db68e27216220cce5e
sken100627 <shigujianren01@gmail.com>
2023-09-01 12:18:17 +0900

    itemモデルバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,16 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+  
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
