3eafcf41a5007540219806f1edeb9993fbc13911
sken100627 <shigujianren01@gmail.com>
2023-09-01 11:19:43 +0900

    Active Storageの導入

--- db/migrate/20230827055258_create_items.rb
+++ db/migrate/20230827055258_create_items.rb
@@ -2,13 +2,13 @@ class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
       t.string  :name,               null: false, default: ""
-      t.text    :explanation,        null: false, default: ""
-      t.integer :category_id,        null: false, default: ""
-      t.integer :condition_id,       null: false, default: ""
-      t.integer :shipping_charge_id, null: false, default: ""
-      t.integer :prefecture_id,      null: false, default: ""
-      t.integer :price,              null: false, default: ""
-      t.integer :user_id,            null: false, default: ""
+      t.text    :explanation,        null: false
+      t.integer :category_id,        null: false
+      t.integer :condition_id,       null: false
+      t.integer :shipping_charge_id, null: false
+      t.integer :prefecture_id,      null: false
+      t.integer :price,              null: false
+      t.integer :user_id,            null: false
 
       t.timestamps
     end
