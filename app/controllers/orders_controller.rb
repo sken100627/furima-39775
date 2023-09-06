2cbf12b2ff2def2def1c89c084986236b0a6a80c
sken100627 <shigujianren01@gmail.com>
2023-09-06 14:32:10 +0900

    クレジット決済完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,19 @@
 class OrdersController < ApplicationController
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @purchase_form = PurchaseForm.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase_form = PurchaseForm.new(order_params)
     if @purchase_form.valid?
+      pay_item
       @purchase_form.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       @item = Item.find(params[:item_id])
       render :index, status: :unprocessable_entity
     end
@@ -18,7 +22,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:purchase_form).permit(:post_code, :prefecture_id, :municipality, :street_address, :building, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_form).permit(:post_code, :prefecture_id, :municipality, :street_address, :building, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      charge = Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 
 end
--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,6 +1,6 @@
 class PurchaseForm
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipality, :street_address, :building, :telephone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :municipality, :street_address, :building, :telephone_number, :user_id, :item_id, :token
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
@@ -11,6 +11,7 @@ class PurchaseForm
     validates :telephone_number, format: {with: /\A\d{10,11}\z/, allow_blank: true}
     validates :user_id
     validates :item_id
+    validates :token, presence: true
   end
 
   def save
