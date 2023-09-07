a7aa34e4bdf123e8ba50c9ecf09d9f2ce723af52
sken100627 <shigujianren01@gmail.com>
2023-09-07 10:14:35 +0900

    重複バリデーション削除

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -3,7 +3,6 @@ class PurchaseForm
   attr_accessor :post_code, :prefecture_id, :municipality, :street_address, :building, :telephone_number, :user_id, :item_id,
                 :token
 
-  # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)', allow_blank: true }
     validates :prefecture_id, numericality: { other_than: 1 }
@@ -12,7 +11,7 @@ class PurchaseForm
     validates :telephone_number, format: { with: /\A\d{10,11}\z/, allow_blank: true }
     validates :user_id
     validates :item_id
-    validates :token, presence: true
+    validates :token
   end
 
   def save
